20091de520f399c43fb7a85dce10425d9a346612
Chih-Chun Chen <chihchunc@gmail.com>
2025-11-20 09:33:17 +0100

    EXERCISE 2: Fetch and save user input

--- flashcard_app.py
+++ flashcard_app.py
@@ -5,10 +5,10 @@
 # Welcome message
 print("Welcome to your personal flashcard trainer!")
 
-# Variables (Task 1)
-name = "Alex Ubuntu"
+# Variables
+name = input("What is your name? ") # Fetch from user and save to variable
 max_cards = 20
 
-# Using f-strings (Task 4)
+# Using f-strings 
 print(f"My name is {name}")
 print(f"I want to practice at most {max_cards} cards per session")
