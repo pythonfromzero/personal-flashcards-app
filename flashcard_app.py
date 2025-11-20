e1392ee09f8a4c286176bddabb7b2bb93e3004f0
Chih-Chun Chen <chihchunc@gmail.com>
2025-11-20 09:27:31 +0100

    EXERCISE 0: Add comments to explain code

--- flashcard_app.py
+++ flashcard_app.py
@@ -1,1 +1,6 @@
+# Created by: Alex Ubuntu
+# Date: 01.01.2026
+# Purpose: A personal flashcard trainer to help with learning
+
+# Welcome message
 print("Welcome to your personal flashcard trainer!")
