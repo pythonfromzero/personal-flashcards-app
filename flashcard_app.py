2fdd0bae26527049f52c3863a60a04e6641ee984
Chih-Chun Chen <chihchunc@gmail.com>
2025-11-20 09:28:50 +0100

    EXERCISE 1: Store user details in variables

--- flashcard_app.py
+++ flashcard_app.py
@@ -4,3 +4,11 @@
 
 # Welcome message
 print("Welcome to your personal flashcard trainer!")
+
+# Variables (Task 1)
+name = "Alex Ubuntu"
+max_cards = 20
+
+# Using f-strings (Task 4)
+print(f"My name is {name}")
+print(f"I want to practice at most {max_cards} cards per session")
